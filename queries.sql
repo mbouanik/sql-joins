a9726bb68f5778e2bbc2a25c1c59c39df2bfefb6
mbouanik <mbouanik@student.42.fr>
2023-09-29 16:22:48 -0500

    quries done

---
+++ queries.sql
@@ -0,0 +1,3 @@
+SELECT *  FROM owners LEFT JOIN vehicles ON vehicles.owner_id = owners.id;
+SELECT first_name, last_name, COUNT(*) FROM owners JOIN vehicles ON vehicles.owner_id = owners.id GROUP BY owners.id ORDER BY first_name;
+SELECT first_name, last_name, ROUND(AVG(price)) AS avg_price, COUNT(*) FROM owners JOIN vehicles ON vehicles.owner_id = owners.id GROUP BY owners.id HAVING AVG(price) > 10000 AND COUNT(*) > 1 ORDER BY first_name DESC;
